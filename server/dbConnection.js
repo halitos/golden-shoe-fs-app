37b5a78936a7d607bb5a67076abdd4dc03be162a
halit <oskan96@gmail.com>
2021-02-19 16:02:19 +0000

    db config modified

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,11 +1,27 @@
 const { Pool } = require("pg");
 
-const db = new Pool({
-  user: "44788",
-  host: "localhost",
-  database: "golden_shoe",
-  password: "*****",
-  port: 5432,
-});
+let pool;
+let db;
 
-module.exports = db;
+if (process.env.DATABASE_URL) {
+  const connectionString = process.env.DATABASE_URL;
+  db = {
+    connectionString: connectionString,
+    sslmode: require,
+    ssl: {
+      rejectUnauthorised: false,
+    },
+  };
+} else {
+  db = {
+    user: "44788",
+    host: "localhost",
+    database: "golden_shoe",
+    password: "*****",
+    port: 5432,
+  };
+}
+
+pool = new Pool(db);
+
+module.exports = pool;
